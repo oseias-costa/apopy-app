cdee53513b71d1aaf7555f6996d3930ca95155fd
Oséias Costa <oseiasc2@gmail.com>
2023-05-24 20:47:40 +0000

    new adress

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://odd-erin-stingray-tutu.cyclic.app/graphql',
-  cache: new InMemoryCache()
-})
+  uri: "https://orchid-cockatoo-hose.cyclic.app/graphql",
+  cache: new InMemoryCache(),
+});
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
