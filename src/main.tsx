9d18de60070b2e162aa10cc1cd5c2881c4a6ef58
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-05-25 08:41:31 -0300

    create: folders

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://orchid-cockatoo-hose.cyclic.app/graphql",
+  uri: "https://apopy-api.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
