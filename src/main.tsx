2c4dee14908c0e66487b932a7f5c0f084580538e
Oséias Costa <oseiasc2@gmail.com>
2023-05-27 20:21:51 +0000

    create: redux store and userSlice

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 const client = new ApolloClient({
   uri: "https://apopy-api.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
+const store = configureStore({
+  reducer: {},
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
   </React.StrictMode>
 );
