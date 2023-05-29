82a625abcfd3d1643415ea925a1604a7d1cc5971
Oséias Costa <oseiasc2@gmail.com>
2023-05-29 23:13:52 +0000

    apollo/client test cache

--- src/main.tsx
+++ src/main.tsx
@@ -5,42 +5,43 @@ import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://apopy-api.onrender.com/graphql",
+  uri: "https://apopy-api.vercel.app/graphql",
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
         fields: {
           category: {
             read: (existing, { toReference, args }) => {
-              const categoryRef = toReference({ __typename: "Category", _id: args._id})
-              return existing ?? categoryRef
-            }
-          }
-        }
+              const categoryRef = toReference({
+                __typename: "Category",
+                _id: args._id,
+              });
+              return existing ?? categoryRef;
+            },
+          },
+        },
       },
       Category: {
-        keyFields: ['_id'],
+        keyFields: ["_id"],
         fields: {
           categoryTest: {
             read: (_, { readField }) => {
-              const name = readField('name')
-              const _id = readField('_id')
+              const name = readField("name");
+              const _id = readField("_id");
 
-              return `${name} ${_id}`
-            }
-          }
-        }
-      }
-    }
+              return `${name} ${_id}`;
+            },
+          },
+        },
+      },
+    },
   }),
 });
 
-console.log(client)
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,83 +1,122 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
-import { CATEGORIES, ADD_CATEGORY, UPDATE_CATEGORY, DELETE_CATEGORY } from "../queries/categories";
+import {
+  CATEGORIES,
+  ADD_CATEGORY,
+  UPDATE_CATEGORY,
+  DELETE_CATEGORY,
+} from "../queries/categories";
 import { Supliers } from "../components/stock/Supliers";
-import { CategoryData, CategoryState } from '../types/category.type'
+import { CategoryData, CategoryState } from "../types/category.type";
 
 export const Products = () => {
-  const [ categoryState, setCategoryState ] = useState<CategoryState>({ _id: '', name: '', type: 'create' })
+  const [categoryState, setCategoryState] = useState<CategoryState>({
+    _id: "",
+    name: "",
+    type: "create",
+  });
   const { data, loading, error } = useQuery(CATEGORIES, {
     variables: {
       userId: "6451a787de4c08d54ed8da35",
     },
   });
-  const [ createCategory, { data: cData, loading: cLoading, error: cError }] = useMutation(ADD_CATEGORY, {
-    update: (cache, { data })=> {
-      // console.log('olhe:', data)
-      cache.modify({
-        fields: {
-          categories: (existingFieldData = []) => {
-              const newCategory = data.createCategory
-              cache.writeQuery({
-                query: CATEGORIES,
-                data: { newCategory, ...existingFieldData }
-              })
-          }
-        }
-      })
-      console.log('deu')
-    }
-  })
-  const [ updateCategory, { data: eData, loading: eLoading, error: eError }] = useMutation(UPDATE_CATEGORY)
-  const [ deleteCategory, { data: dData, loading: dLoading, error: dError }] = useMutation(DELETE_CATEGORY)
-  
-  const handleMutation = async (categoryState: CategoryState, setCategoryState: React.Dispatch<React.SetStateAction<CategoryState>>) => {
-    if (categoryState.type === 'create') {
-       await createCategory({
+  const [createCategory, { data: cData, loading: cLoading, error: cError }] =
+    useMutation(ADD_CATEGORY, {
+      update: (cache, { data: { createCategory } }) => {
+        const { categories } = cache.readQuery({ query: CATEGORIES });
+
+        cache.writeQuery({
+          query: CATEGORIES,
+          data: { createCategory, ...categories },
+        });
+        // console.log('olhe:', data)
+        // cache.modify({
+        // fields: {
+        //categories: (existingFieldData = []) => {
+        // const newCategory = data.createCategory
+        // cache.writeQuery({
+        // query: CATEGORIES,
+        //  data: { newCategory, ...existingFieldData }
+        //     })
+        //  }
+        // }
+        //})
+      },
+    });
+  const [updateCategory, { data: eData, loading: eLoading, error: eError }] =
+    useMutation(UPDATE_CATEGORY, {
+      update: (cache, { data }) => {  
+        cache.updateQuery({ query: CATEGORIES}, (prev) => ({
+          console.log(prev)
+        }))
+      },
+    });
+
+  const [deleteCategory, { data: dData, loading: dLoading, error: dError }] =
+    useMutation(DELETE_CATEGORY);
+
+  const handleMutation = async (
+    categoryState: CategoryState,
+    setCategoryState: React.Dispatch<React.SetStateAction<CategoryState>>
+  ) => {
+    if (categoryState.type === "create") {
+      await createCategory({
         variables: {
           categoryInput: {
             name: categoryState.name,
             userId: "6451a787de4c08d54ed8da35",
-          }
-        }
-       })
-
-    } else if (categoryState.type === 'update') {
+          },
+        },
+      });
+    } else if (categoryState.type === "update") {
       await updateCategory({
         variables: {
           categoryEdit: {
             _id: categoryState._id,
-            name: categoryState.name
-          }
-        }
-      })
+            name: categoryState.name,
+          },
+        },
+      });
     } else {
       await deleteCategory({
         variables: {
           categoryEdit: {
-            _id: categoryState._id
-          }
-        }
-      })
+            _id: categoryState._id,
+          },
+        },
+      });
     }
-  }
+  };
 
-  const listProducts = !loading &&
+  const listProducts =
+    !loading &&
     data?.categories.map((item: CategoryData) => {
       return (
         <div key={item._id}>
           <strong>{item.name}</strong>
-          <button onClick={() => setCategoryState({ 
-            _id: item._id, 
-            name: item.name, 
-            type: 'update'
-          })}>Editar</button>
+          <button
+            onClick={() =>
+              setCategoryState({
+                _id: item._id,
+                name: item.name,
+                type: "update",
+              })
+            }
+          >
+            Editar
+          </button>
 
-          <button onClick={() => setCategoryState({ 
-            _id: item._id, 
-            name: item.name, 
-            type: 'delete' 
-          })}>Excluir</button>
+          <button
+            onClick={() =>
+              setCategoryState({
+                _id: item._id,
+                name: item.name,
+                type: "delete",
+              })
+            }
+          >
+            Excluir
+          </button>
 
           {item.subcategory?.map((sub: string) => (
             <p key={sub}>{sub}</p>
@@ -86,19 +125,33 @@ export const Products = () => {
       );
     });
 
-    console.log('query', data)
+  console.log("query", data);
   return (
     <div>
       <h1>Products</h1>
       <div>
-      <h2>Criar</h2>
-      <input 
-        type='text' 
-        value={categoryState.name} 
-        onChange={(e)=> setCategoryState({...categoryState, name: e.target.value}) } 
+        <h2>Criar</h2>
+        <input
+          type="text"
+          value={categoryState.name}
+          onChange={(e) =>
+            setCategoryState({ ...categoryState, name: e.target.value })
+          }
         />
-      <button type='submit' onClick={() => handleMutation(categoryState, setCategoryState)}>{categoryState.type}</button>
-      <button type='submit' onClick={() => setCategoryState({ _id: '', name: '', type: 'create' })}>Desfazer</button>
+        <button
+          type="submit"
+          onClick={() => handleMutation(categoryState, setCategoryState)}
+        >
+          {categoryState.type}
+        </button>
+        <button
+          type="submit"
+          onClick={() =>
+            setCategoryState({ _id: "", name: "", type: "create" })
+          }
+        >
+          Desfazer
+        </button>
       </div>
       <div>{listProducts}</div>
       <h2>Fornecedores</h2>
