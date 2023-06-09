949628b675f9bf7696474685070b10ad54d7494f
Oséias Costa <oseiasc2@gmail.com>
2023-06-09 19:20:50 +0000

    update: custom hook useMutateState types

--- src/pages/managementProducts/components/mutate-state.ts
+++ src/pages/managementProducts/components/mutate-state.ts
@@ -1,29 +1,33 @@
-import { useState, useEffect } from 'react'
-import { makeVar, useReactiveVar } from "@apollo/client"
+import { useState } from "react";
+import { makeVar, useReactiveVar } from "@apollo/client";
 
-type mutateProps = 
-    { _id: string; name: string; type: string; edit: string; }
+type mutateProps = {
+  _id: string;
+  name: string;
+  type: string;
+  edit: string;
+  categoryName?: string;
+  openModal: boolean;
+};
 
+export const initialValue = {
+  _id: "",
+  name: "",
+  type: "create",
+  edit: "",
+  categoryName: "",
+  openModal: false,
+};
 export const useMutateState = () => {
-    const initialValue = {
-        _id: "",
-        name: "",
-        type: "create",
-        edit: ""
-    }
-    const [ state, setState ] = useState(initialValue)
+  const [state, setState] = useState<mutateProps>(initialValue);
 
-    const mutateStateFn = makeVar(state)
-    useReactiveVar(mutateStateFn)
+  const mutateStateFn = makeVar(state);
+  useReactiveVar(mutateStateFn);
 
-    const dispatch = (newState: mutateProps) => {
-        // const newState = reducer(state)
-        setState(newState)
-    }
+  const dispatch = (newState: mutateProps) => {
+    // const newState = reducer(state)
+    setState(newState);
+  };
 
-    if(!state) {
-        setState(initialValue)
-    }
-
-    return { state, dispatch: (s: mutateProps) => dispatch(s) }
-}
+  return { state, dispatch: (s: mutateProps) => dispatch(s) };
+};
