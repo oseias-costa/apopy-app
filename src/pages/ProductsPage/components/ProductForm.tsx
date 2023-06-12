091335761a5b16f466ad01a22ad175c90470db19
Oséias Costa <oseiasc2@gmail.com>
2023-06-12 14:47:32 +0000

    fixed: category selected

--- src/pages/ProductsPage/components/ProductForm.tsx
+++ src/pages/ProductsPage/components/ProductForm.tsx
@@ -1,24 +1,52 @@
-import { useQuery } from "@apollo/client"
-import { CATEGORIES } from "../../../queries/categories"
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { CATEGORIES } from "../../../queries/categories";
 
 export const ProductForm = () => {
-    const { data } = useQuery(CATEGORIES, {
-        variables: {
-          userId: "6451a787de4c08d54ed8da35",
-        },
-      })
-    console.log(data)
-    return(
-        <div>
-            <select>
-                { data?.categories.map((item) => {
-                    return(
-                        <div>                           
-                            <option>{item.name}</option>
-                        </div>
-                    )
-                })}
-            </select>
-        </div>
-    )
-}
+  const [itemSelected, setItemSelected] = useState({
+    category: "",
+    subcategory: "",
+    suplier: "",
+    product: "",
+    newProduct: "",
+  });
+  console.log(itemSelected);
+  const { data } = useQuery(CATEGORIES, {
+    variables: {
+      userId: "6451a787de4c08d54ed8da35",
+    },
+  });
+  
+  const categorySelect = (
+      <select
+        onChange={(e) =>
+          setItemSelected({ ...itemSelected, category: e.target.value })
+        }
+      >
+        {data?.categories.map((item) => (
+          <option key={item.name} value={item.name}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+  )
+  
+  const subcategorySelect = (
+      <select>
+          { itemSelected.category !== '' && 
+                { data?.categories.itemSelected.Category.map((item: string) => (
+                    <option>{item}</option>   
+                ))     
+                }
+                
+          
+           }
+      </select>
+  )
+  
+  return (
+    <div>
+        { categorySelect }
+    </div>
+  );
+};
